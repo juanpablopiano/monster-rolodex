a1bdce84f600ae75c9b22214cd42f368268802d3
Juan Gonzalez <jgu1@hotmail.com>
2020-07-04 13:37:16 -0400

    Updates

---
+++ precache-manifest.8e8da8e8987c77b25472710bc4811323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2d3f21401bab30cac8971a0f680dce",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ee0e6209c513e330fef1",
+    "url": "/monster-rolodex/static/css/main.44d6f2fc.chunk.css"
+  },
+  {
+    "revision": "f7e4a90421a606ce548a",
+    "url": "/monster-rolodex/static/js/2.0c186829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.0c186829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0e6209c513e330fef1",
+    "url": "/monster-rolodex/static/js/main.acbd8306.chunk.js"
+  },
+  {
+    "revision": "6ccb4c20baf9645a1201",
+    "url": "/monster-rolodex/static/js/runtime-main.5288bf96.js"
+  }
+]);
